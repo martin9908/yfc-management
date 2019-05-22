8bbd0e830a20fceb3e992e4c6c39aabd086ee48a
Martin Jaycy Halum <>
2019-05-22 12:49:14 +0800

    members

--- pages/user/household_create.php
+++ pages/user/household_create.php
@@ -7,6 +7,7 @@ $connect = mysqli_connect($host, $user, $pass,$databasename) or die("Couldn't co
 
 //SQL Scripts
 $household = mysqli_query($connect, "SELECT * FROM info_user");
+$users = mysqli_query($connect, "SELECT * FROM info_user");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -70,6 +71,13 @@ $household = mysqli_query($connect, "SELECT * FROM info_user");
                             <option value="<?PHP echo $row['id']; ?>"><?PHP echo $row['First_Name']. " ". $row['Last_Name']?></option>
                         <?PHP }?>
                     </select>
+                    <br />
+                    <label>Select Members</label>
+                    <div style="height:250px;width:350px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+                      <?PHP  while($row1 = mysqli_fetch_assoc($users)){?>
+                        <input type="checkbox" name="check_list[]" value="<?PHP echo $row1['id']; ?>"><?PHP echo $row1['First_Name']. " ". $row1['Last_Name']?><br/>
+                      <?PHP }?>
+                    </div>
                     <br>
                     <center>
                       <button type="submit" class="btn btn-success">Save</button>
--- pages/user/household_edit.php
+++ pages/user/household_edit.php
@@ -82,6 +82,13 @@ while($row = mysqli_fetch_assoc($household)){
                             <?PHP }?>
                         <?PHP }?>
                     </select>
+                    <br />
+                    <label>Select Members</label>
+                    <div style="height:250px;width:350px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+                      <?PHP  while($row1 = mysqli_fetch_assoc($users)){?>
+                        <input type="checkbox" name="check_list[]" value="<?PHP echo $row1['id']; ?>"><?PHP echo $row1['First_Name']. " ". $row1['Last_Name']?><br/>
+                      <?PHP }?>
+                    </div>
                     <br>
                     <center>
                       <button type="submit" class="btn btn-success">Save</button>
